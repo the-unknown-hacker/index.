6d5565a8780245b4d014d95729f279a1738d04eb
the-unknown-hacker <theunknownhacker10010@gmail.com>
2025-11-22 20:16:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function send_mail() {
+    const subject = document.queryselector('input{placeholder="Subject"]').value;
     const name = document.querySelector('input[placeholder="Name"]').value;
     const email = document.querySelector('input[placeholder="Email"]').value;
     const message = document.querySelector('textarea[placeholder="Message"]').value;
-    const subject = document.queryselector('input{placeholder="Subject"]').value;
 
     if (!name || !email || !message || !subject) {
         alert("Fill all the fields.");
         return;
     }
 
-    emailjs.send("service_6w88c3i", "template_mob4dza", {
+    emailjs.send("service_6w88c3i", "template_mob4dza", 
+        user_subject: subject,
         user_name: name,
         user_email: email,
-        user_message: message,
-        user_subject: subject
+        user_message: message
     })
     .then(() => {
         alert("Message sent successfully!");
